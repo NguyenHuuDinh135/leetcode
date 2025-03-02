c7b5295a620ba3e1c81c0119ee2f4dd450495d7b
Dinh <149035922+chanem794@users.noreply.github.com>
2025-03-02 13:14:16 +0700

    Time: 35 ms (96.84%), Space: 53.4 MB (10.07%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Function}
+ */
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+ const f = createHelloWorld();
+console.log(f()); // "Hello World"
+console.log(f({}, null, 42)); // "Hello World"
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
