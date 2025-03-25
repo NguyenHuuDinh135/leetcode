126c9c15777464787bd53d371a62bb6a6e72f77e
Dinh <149035922+chanem794@users.noreply.github.com>
2025-03-25 19:41:30 +0700

    Time: 0 ms (100%), Space: 12.3 MB (86.91%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -10,7 +10,7 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """ 
         current = head
-        while current and current.next:
+        while current != None and current.next != None: 
             if current.val == current.next.val:
                 current.next = current.next.next
             else:
