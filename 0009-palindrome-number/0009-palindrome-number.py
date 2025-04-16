928919e9b5efcc188b64e2a85e17a3ab22006de6
Dinh <149035922+chanem794@users.noreply.github.com>
2025-04-16 20:35:01 +0700

    Time: 29 ms (8.23%), Space: 12.6 MB (11.1%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        dau = 0
+        cuoi = -1
+        if x < 0:
+            return False
+        list = []
+        while x > 0:
+            list.append(x % 10)
+            x = x // 10
+        list.reverse()
+        if len(list) == 1:
+            return True
+        if len(list) == 2 and list[dau] != list[cuoi]:
+            return False
+        for i in range(len(list) // 2):
+            if list[i] != list[-i-1]:
+                return False
+        return True
