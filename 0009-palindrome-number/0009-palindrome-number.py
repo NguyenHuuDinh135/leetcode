5262df704776b3c32505c980bf68c1a8b02958cd
Dinh <149035922+chanem794@users.noreply.github.com>
2025-04-16 20:37:19 +0700

    Time: 9 ms (61.92%), Space: 12.3 MB (82.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        dau = 0
-        cuoi = -1
         if x < 0:
             return False
         list = []
@@ -14,7 +12,7 @@ class Solution(object):
             x = x // 10
         if len(list) == 1:
             return True
-        if len(list) == 2 and list[dau] != list[cuoi]:
+        if len(list) == 2 and list[0] != list[-1]:
             return False
         for i in range(len(list)):
             if list[i] != list[-i-1]:
