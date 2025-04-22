077619cc32ea09c0b5a74f4e4f5904cffd33010c
Dinh <149035922+chanem794@users.noreply.github.com>
2025-04-22 21:06:54 +0700

    Time: 0 ms (100%), Space: 12.5 MB (74.12%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,24 +10,27 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        list= []
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-
-        if list1 is None and list2 is None:
-            return []
-        while list1 is not None:
-            list.append(list1.val)
-            list1 = list1.next
-        while list2 is not None:    
-            list.append(list2.val)
-            list2 = list2.next
-        list.sort()
         head = ListNode(0)
         current = head
-        for i in list:
-            current.next = ListNode(i)
+        if list1 is None and list2 is None:
+            return None
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
             current = current.next
+        if list1 is not None:
+            current.next = list1
+        if list2 is not None:
+            current.next = list2
+            
         return head.next
+            
+            
+            
+            
+
+        
