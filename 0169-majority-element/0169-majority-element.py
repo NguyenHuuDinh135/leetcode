9c7924d207cc3ee78fd1c3045cbd846c87778f2e
Dinh <149035922+chanem794@users.noreply.github.com>
2025-04-26 18:32:57 +0700

    Time: 15 ms (29.69%), Space: 13.8 MB (17.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,9 +5,23 @@ class Solution(object):
         :rtype: int
         """
         # Gặp Time Limit Exceeded
-        # for i in nums:
+        # for i in nums:// Cách 1
         #     if nums.count(i) > len(nums) // 2:
         #         return i
 
-        result = set(nums)
-        return max(result, key = nums.count)
+        # result = set(nums) //cách 2
+        # return max(result, key = nums.count) O(n^2) ko tuyen tinh
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
+        
