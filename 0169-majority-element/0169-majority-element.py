4ba5ac05a4062ab9c495546cca79306c1929bcef
Dinh <149035922+chanem794@users.noreply.github.com>
2025-04-29 21:19:28 +0700

    Time: 5 ms (74.75%), Space: 13.7 MB (63.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -9,19 +9,19 @@ class Solution(object):
         #     if nums.count(i) > len(nums) // 2:
         #         return i
 
-        # result = set(nums) //cách 2
-        # return max(result, key = nums.count) O(n^2) ko tuyen tinh
-        count = 0
-        candidate = None
-        #Boyer-Moore Majority Vote Algorithm.
-        for num in nums:
-            if count == 0:
-                candidate = num
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
+        result = set(nums)
+        return max(result, key = nums.count)
+        # count = 0
+        # candidate = None
+        # #Boyer-Moore Majority Vote Algorithm.
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+        #     if num == candidate:
+        #         count += 1
+        #     else:
+        #         count -= 1
 
-        return candidate
+        # return candidate
 
         
