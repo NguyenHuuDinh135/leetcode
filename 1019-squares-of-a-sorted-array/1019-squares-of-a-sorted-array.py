cf8ef445a82551b586414c480a69ad447c3126e9
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-19 12:10:10 +0700

    Time: 12 ms (58.99%), Space: 14.7 MB (6.58%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -11,12 +11,12 @@ class Solution(object):
 
         left = 0
         right = len(nums) - 1
-        res = []
-        while len(res) < len(nums):
+        res = [0] * len(nums)
+        for i in range(len(res) - 1, -1, -1):
             if abs(nums[left]) > abs(nums[right]):
-               res.append(nums[left] ** 2)
+               res[i] = nums[left] ** 2
                left += 1
             else:
-                res.append(nums[right] ** 2)    
+                res[i] = nums[right] ** 2
                 right -= 1
-        return res[::-1]
+        return res
