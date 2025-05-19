3d2126bc653e21259426eb8bee08c36f8d427713
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-19 18:30:52 +0700

    Time: 35 ms (87.06%), Space: 12.8 MB (4.92%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        temp = nums[n : len(nums)]
+        res = []
+        for i in range(0, n):
+            res.append(nums[i])
+            res.append(temp[i])
+        return res
+
