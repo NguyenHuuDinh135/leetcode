59681c5bb498851061121fb1e230a16958619ab2
Dinh <149035922+chanem794@users.noreply.github.com>
2025-05-27 21:17:55 +0700

    Time: 8 ms (80.93%), Space: 14.4 MB (16.77%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -4,19 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            nums[i] = nums[i] ** 2
-        nums.sort()
-        return nums
+        # for i in range(len(nums)):
+        #     nums[i] = nums[i] ** 2
+        # nums.sort()
+        # return nums
 
-        # left = 0
-        # right = len(nums) - 1
-        # res = [0] * len(nums)
-        # for i in range(len(res) - 1, -1, -1):
-        #     if abs(nums[left]) > abs(nums[right]):
-        #        res[i] = nums[left] ** 2
-        #        left += 1
-        #     else:
-        #         res[i] = nums[right] ** 2
-        #         right -= 1
-        # return res
+        left = 0
+        right = len(nums) - 1
+        res = [0] * len(nums)
+        for i in range(len(res) - 1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+               res[i] = nums[left] ** 2
+               left += 1
+            else:
+                res[i] = nums[right] ** 2
+                right -= 1
+        return res
