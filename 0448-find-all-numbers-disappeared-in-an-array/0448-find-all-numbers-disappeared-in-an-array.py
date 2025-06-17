90a64953a9c0303942106911f9e91117583b11c9
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-06-17 19:35:04 +0700

    Time: 28 ms (85.81%), Space: 26.8 MB (70.64%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,7 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            while nums[nums[i]-1] != nums[i]: 
-                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
-        return [i for i in range(1, len(nums)+1) if i != nums[i-1]]
+        new = set(nums)
+        return [i for i in range(1, len(nums)+1) if i not in new]
