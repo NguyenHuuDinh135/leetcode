973b830eb1cfd7044746bf87a06eee9682bf6e46
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-08-22 23:16:24 +0700

    Time: 11 ms (44.09%), Space: 13.4 MB (20.39%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -9,13 +9,16 @@ class Solution(object):
             return 0
         
         steps = 0
-        curr_end = 0
-        curr_farthest = 0
+        end = 0
+        farthest = 0
         
         for i in range(n-1):
-            curr_farthest = max(curr_farthest, i + nums[i])
-            if i == curr_end:
+            farthest = max(farthest, i + nums[i])
+            if farthest >= len(nums) - 1:
                 steps += 1
-                curr_end = curr_farthest
+                break
+            if i == end:
+                steps += 1 
+                end = farthest
         
         return steps
