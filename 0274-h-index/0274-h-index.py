2972f56fec33dc239d0f7e5dfe955715ca06ec12
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-08-23 17:52:48 +0700

    Time: 0 ms (100%), Space: 12.7 MB (34.29%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # Có nghĩa là trong các bài báo có trích dẫn thì nó trích dẫn ít nhất h lần
+        citations.sort(reverse=True)
+        h = 0
+        for i, citation in enumerate(citations):
+            if citation >= i + 1 :
+                h = i + 1
+        return h
