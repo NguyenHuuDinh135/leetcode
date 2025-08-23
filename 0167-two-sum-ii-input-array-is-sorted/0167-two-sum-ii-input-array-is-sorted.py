c7fbb6921591b7fa3134371ec9c5820f8df26412
NguyenHuuDinh <149035922+chanem794@users.noreply.github.com>
2025-08-23 18:45:15 +0700

    Time: 2 ms (90.14%), Space: 12.9 MB (69.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         num_to_index = {}
         for i, num in enumerate(numbers):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i + 1]
-            num_to_index[num] = i + 1
+            key_in_dic = target - num #Ex: complement = 9 - 2,  complement = 9 - 7 = 2
+            if key_in_dic in num_to_index:
+                return [num_to_index[key_in_dic], i + 1]
+            num_to_index[num] = i + 1 # num_to_index = {2: 1}
