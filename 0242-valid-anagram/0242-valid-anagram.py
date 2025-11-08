c98a1f85fab88607060fbd3e9fc82f47d587fcc4
NguyenHuuDinh <149035922+NguyenHuuDinh135@users.noreply.github.com>
2025-11-08 09:39:58 +0700

    Time: 23 ms (55.55%), Space: 12.3 MB (99.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,19 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-
-        count_s = {}
-        count_t = {}
-        for c in s:
-            if c in count_s:
-                count_s[c] += 1
-            else:
-                count_s[c] = 1
-        for c in t:
-            if c in count_t:
-                count_t[c] += 1
-            else:
-                count_t[c] = 1
-        return count_s == count_t
+        return Counter(s) == Counter(t)
