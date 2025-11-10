bb5b8ec865145c20dd429d5e374ab76715e3d022
NguyenHuuDinh <149035922+NguyenHuuDinh135@users.noreply.github.com>
2025-11-10 10:04:39 +0700

    Time: 17 ms (87.55%), Space: 16.2 MB (55.56%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            res[key].append(s)
+        return res.values()
