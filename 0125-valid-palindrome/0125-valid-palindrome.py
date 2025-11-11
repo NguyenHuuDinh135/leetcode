c0000dd29c19b0f95fd92153cc25fd5b34034260
NguyenHuuDinh <149035922+NguyenHuuDinh135@users.noreply.github.com>
2025-11-11 10:37:40 +0700

    Time: 11 ms (88.08%), Space: 14.1 MB (34.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,12 +6,4 @@ class Solution(object):
         """
         s = ''.join(c.lower() for c in s if c.isalnum())
 
-        l = 0
-        r = len(s) - 1
-
-        while l < r:
-            if s[l] != s[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+        return s == s[::-1]
